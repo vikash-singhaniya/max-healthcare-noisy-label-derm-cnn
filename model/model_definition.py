26fa5cf59fef90787f9e0cb927ef4ddc963e8344
vikash-singhaniya <38317339+vikash-singhaniya@users.noreply.github.com>
2026-02-03 03:58:51 +0530

    Create model_definition.py

---
+++ model/model_definition.py
@@ -0,0 +1,38 @@
+import torch.nn as nn
+
+class DermCNN(nn.Module):
+    def __init__(self, num_classes=7):
+        super().__init__()
+
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(1, 32, kernel_size=3, padding=1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.MaxPool2d(2)
+        )
+
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(32, 64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.MaxPool2d(2)
+        )
+
+        self.conv3 = nn.Sequential(
+            nn.Conv2d(64, 128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU()
+        )
+
+        self.dropout = nn.Dropout(0.5)
+        self.gap = nn.AdaptiveAvgPool2d(1)
+        self.fc = nn.Linear(128, num_classes)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.conv3(x)
+        x = self.gap(x)
+        x = x.view(x.size(0), -1)
+        x = self.dropout(x)
+        return self.fc(x)
