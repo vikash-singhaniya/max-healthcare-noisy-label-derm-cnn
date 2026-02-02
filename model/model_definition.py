ee939b2646741be481ab3d02590efb2837d87d85
vikash-singhaniya <38317339+vikash-singhaniya@users.noreply.github.com>
2026-02-03 04:34:39 +0530

    Update model_definition.py

--- model/model_definition.py
+++ model/model_definition.py
@@ -5,21 +5,21 @@ class DermCNN(nn.Module):
         super().__init__()
 
         self.conv1 = nn.Sequential(
-            nn.Conv2d(1, 32, kernel_size=3, padding=1),
+            nn.Conv2d(1, 32, 3, padding=1),
             nn.BatchNorm2d(32),
             nn.ReLU(),
             nn.MaxPool2d(2)
         )
 
         self.conv2 = nn.Sequential(
-            nn.Conv2d(32, 64, kernel_size=3, padding=1),
+            nn.Conv2d(32, 64, 3, padding=1),
             nn.BatchNorm2d(64),
             nn.ReLU(),
             nn.MaxPool2d(2)
         )
 
         self.conv3 = nn.Sequential(
-            nn.Conv2d(64, 128, kernel_size=3, padding=1),
+            nn.Conv2d(64, 128, 3, padding=1),
             nn.BatchNorm2d(128),
             nn.ReLU()
         )
@@ -36,3 +36,4 @@ class DermCNN(nn.Module):
         x = x.view(x.size(0), -1)
         x = self.dropout(x)
         return self.fc(x)
+
